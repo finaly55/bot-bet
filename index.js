96cb314d1a9c08e644d55a429c1f986903d11727
Julien LEBATARD <julien@MacBook-Pro-de-Admin-4.local>
2022-07-05 23:08:27 +0200

    Cote ok, betsure ok

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ chromeOptions.addArguments("--disable-popup-blocking");
 chromeOptions.addArguments("--disable-default-apps");
 chromeOptions.addArguments("--disable-notifications")
 chromeOptions.addArguments("--disable-infobars");
-chromeOptions.addArguments("--user-data-dir=C:/Users/Julien/AppData/Local/Google/Chrome/User Data/Profile 1");
+// chromeOptions.addArguments("--user-data-dir=/Users/julien/Library/Application Support/Google/Chrome/Default");
 
 
 let cotes = []
@@ -24,37 +24,70 @@ async function runTestWithCaps() {
 
     try {
         await driver.get("https://www.hltv.org/");
+
+        // acceptation cookies
         try {
             await driver.findElement(webdriver.By.xpath('//*[@id="CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll"]')).click()
         } catch (e) {
             console.log("cookie déjà accepté")
         }
 
-        var links = await driver.findElements(webdriver.By.className("hotmatch-box"))
-
-        await links[5].click()
-
-        let cotes1 = await driver.findElements(webdriver.By.css('#betting  table > tbody > tr > td:nth-child(2) > a')).map(async cote=> await cote.getText())
-
-
-        let cotes2 = await driver.findElements(webdriver.By.css('#betting  table > tbody > tr > td:nth-child(4) > a')).map(async cote=> await cote.getText())
-
-        console.log(cotes1)
-        console.log(cotes2)
-
-
-        let cote1Max = Math.max.apply(Math, cotes1);
-        let cote2Max = Math.max.apply(Math, cotes2);
-
-        cotes.push({
-            link: links[5],
-            cote1Max: cote1Max,
-            cote2Max: cote2Max,
+        // recuperation des liens des matchs
+        let links = await driver.findElements(webdriver.By.className("hotmatch-box"))
+        let tabs = links.map(async links => {
+            return links.getAttribute("href").then((value) => { return value; })
         })
+        let linksResult = await Promise.all(tabs);
+        linksResult = linksResult.filter(result => !result.includes("play-in"))
 
+        // aller sur le lien
+        for (let i = 0; i < linksResult.length; i++) {
+            await driver.get(linksResult[i]);
+            await sleep(randomIntFromInterval(700, 1200))
+
+            // recuperation des cotes de la premiere team
+            let cotes1 = await driver.findElements(webdriver.By.css('#betting  table > tbody > tr > td:nth-child(2) > a'))
+            let cotesText = cotes1.map(async cote => {
+                return cote.getText().then(value => {
+                    if (!isNaN(parseFloat(value)) && !(/[a-z]/i.test(value))) {
+                        return parseFloat(value)
+                    }
+                })
+            })
+
+            let results1 = await Promise.all(cotesText);
+
+
+            // recuperation des cotes de la premiere team
+            let cotes2 = await driver.findElements(webdriver.By.css('#betting  table > tbody > tr > td:nth-child(4) > a'))
+            cotesText = cotes2.map(async cote => {
+                return cote.getText().then(value => {
+                    if (!isNaN(parseFloat(value)) && !(/[a-z]/i.test(value))) {
+                        return parseFloat(value)
+                    }
+                })
+            })
+            let results2 = await Promise.all(cotesText);
+
+            // on retire toutes les cotes undefined
+            results1 = results1.filter(result => result != undefined)
+            results2 = results2.filter(result => result != undefined)
+
+            // recuperation des cotes les plus hautes
+            let cote1Max = Math.max.apply(Math, results1);
+            let cote2Max = Math.max.apply(Math, results2);
+
+            // ajout des cotes et du lien du bet dans le tableau des bets
+            cotes.push({
+                link: linksResult[i],
+                cote1Max: cote1Max,
+                cote2Max: cote2Max,
+                betsure: 1 - (1/cote1Max) + (1/cote2Max)
+            })
+        }
+        cotes = cotes.filter(cote => cote.betsure > 0)
         console.log(cotes)
 
-        await new Promise(resolve => setTimeout(resolve, 3000)); // 3 sec
 
     } catch (e) {
         console.log("Error:", e.message)
@@ -62,3 +95,13 @@ async function runTestWithCaps() {
     await driver.quit();
 }
 runTestWithCaps();
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
